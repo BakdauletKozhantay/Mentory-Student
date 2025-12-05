ff39811c233d0e13ef7324af879a5faa3901a133
BakdauletKozhantay <samal.city1997@gmail.com>
2025-12-05 23:05:20 +0500

    Сделал домашку Лисон 3

---
+++ Leason10/script.js
@@ -0,0 +1,21 @@
+
+
+const modalOpenBtns = document .querySelectorALL('.open-modal');
+
+const modalCloseBtn =document.querySelector('.close-modal')
+
+// console.log(document);
+
+const modal = document.querySelectorALL('.modal')
+
+modalOpenBtn.addEventListener('click'() => {
+    modal.classList.remove('open')
+});
+
+modalOpenBtns.array.forEach(element => {
+    
+});
+
+modalOpenBtn.addEventListener('click'() => {
+    modal.classList.add('open')
+});
