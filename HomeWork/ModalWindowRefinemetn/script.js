3b126a5bce74086032c43d5574f2b29a2e8a2bcf
BakdauletKozhantay <samal.city1997@gmail.com>
2025-12-21 15:07:46 +0500

    Задал всем индекс

--- HomeWork/ModalWindowRefinemetn/script.js
+++ HomeWork/ModalWindowRefinemetn/script.js
@@ -1,53 +1,11 @@
-const openBtn = document.querySelector(".open-modal");
-const modalOverlay = document.getElementById("modalOverlay");
-const closeBtn = document.getElementById("closeBtn");
+// Закрытие кликом вне модального окна
+const windowClose = document.querySelector(".container");
 
-openBtn.addEventListener("click", () => {
-  modalOverlay.style.display = "block";
-});
-
-closeBtn.addEventListener("click", () => {
-  modalOverlay.style.display = "none";
-});
-
-modalOverlay.addEventListener("click", (event) => {
-  if (event.target === modalOverlay) {
-    modalOverlay.style.display = "none";
-  }
-});
-
-
-// --- АККОРДИОН ---
-// Находим все заголовки аккордионов
-const accordionHeaders = document.querySelectorAll(".accordion__header");
-
-accordionHeaders.forEach(header => {
-  header.addEventListener("click", () => {
-    const block = header.parentElement;     // .accordion__block
-    const state = block.getAttribute("accordion-state");
-
-    // Если открыт → закрываем
-    if (state === "open") {
-      block.setAttribute("accordion-state", "closed");
-    }
-    // Если закрыт → открываем
-    else {
-      block.setAttribute("accordion-state", "open");
+function closeModalOverview(event) {
+  modalWindows.forEach((modal) => {
+    if (event.target === modal) {
+      modal.classList.remove("open");
     }
   });
-});
-
-
-
-
-
-
-
-
-
-
-console.log("JS работает!");
-
-console.log("openBtn:", document.querySelector(".open-modal"));
-console.log("modalOverlay:", document.getElementById("modalOverlay"));
-console.log("closeBtn:", document.getElementById("closeBtn"));
+}
+windowClose.addEventListener("click", closeModalOverview);
