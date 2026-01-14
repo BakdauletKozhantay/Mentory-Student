92626c34e2514300e0148f2353e55ca7dd268d9c
Bakdaulet <mr.baha_1995@mail.ru>
2026-01-14 12:48:40 +0500

    Улучшил

--- YrysAuToGas/script.js
+++ YrysAuToGas/script.js
@@ -9,3 +9,28 @@ document.querySelectorAll('.btn').forEach(btn => {
 });
 
 console.log("Скрипт подключен!");
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute('href');
+    const target = document.querySelector(targetId);
+
+    if (target) {
+      target.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
+
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) { // если прокрутили больше 50px
+    header.classList.add('header--scrolled'); // добавляем класс
+  } else {
+    header.classList.remove('header--scrolled'); // убираем класс
+  }
+});
