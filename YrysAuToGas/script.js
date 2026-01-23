bcc51d5511da96a98b587dfbf0f74450a5260cdb
BagiVS <samal.city1997@gmail.com>
2026-01-23 22:30:38 +0500

    Мы вставили сертификат и добавили жс

--- YrysAuToGas/script.js
+++ YrysAuToGas/script.js
@@ -20,3 +20,41 @@ window.addEventListener('scroll', () => {
   if (!header) return;
   header.classList.toggle('header--scrolled', window.scrollY > 50);
 });
+
+
+const certModal = document.getElementById("certModal");
+const certModalImg = document.getElementById("certModalImg");
+const certModalTitle = document.getElementById("certModalTitle");
+
+document.querySelectorAll(".cert-card").forEach((card) => {
+  card.addEventListener("click", ()=> {
+    const img = card.dataset.img;
+    const title = card.dataset.title || "Сертификат";
+    
+    certModalImg.src = img;
+    certModalImg.alt = title;
+    certModalTitle.textContent = title;
+
+    certModal.classList.add("is-open");
+    certModal.setAttribute("aria-hidden", "false");
+    document.body.style.overflow = "hidden";
+  })
+});
+
+certModal.addEventListener("click", (e) =>{
+  if (e.target.dataset.close === "true") {
+    certModal.classList.remove("is-open");
+    certModal.setAttribute("aria-hidden", "true");
+    certModalImg.src = "";
+    document.body.style.overflow = "";
+  }
+});
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && certModal.classList.contains("is-open")){
+    certModal.classList.remove("is-open");
+    certModal.setAttribute("aria-hiddden", "true");
+    certModalImg.src = "";
+    document.body.style.overflow = "";
+  }
+});
